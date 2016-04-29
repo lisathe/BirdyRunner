5c659141712c7e2b2e4a51c15a92b46787669250
Lisa The <lisathe97@gmail.com>
2016-04-29 13:37:45 +0200

    Add player movement - Scrolling background script - PlayerMovementscript - Player flying animations

---
+++ BirdyRunner/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float jumpForce;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Jump ();
+	}
+
+	private void Jump()
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			rb.velocity = new Vector2 (rb.velocity.x, jumpForce * Time.deltaTime);
+			Debug.Log("Jumping!");
+		}
+			
+	}
+
+
+}
---
+++ BirdyRunner/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour {
+
+	public float scrollSpeed;
+	public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Scroll ();
+	}
+
+	void Scroll()
+	{
+		//Moving horizontally
+		Vector2 offset = new Vector2 (Time.time * scrollSpeed, 0);
+		rend.material.mainTextureOffset = offset;
+
+	}
+}
