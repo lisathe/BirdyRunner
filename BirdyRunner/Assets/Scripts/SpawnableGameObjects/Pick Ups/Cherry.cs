316f046e085e56cdc75ff211444f85ef8cf05a4c
Lisa The <lisathe97@gmail.com>
2016-05-09 09:22:47 +0200

    Add score system - Add cherry pick up object - Add score system controller

---
+++ BirdyRunner/Assets/Scripts/SpawnableGameObjects/Pick Ups/Cherry.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cherry : MonoBehaviour {
+		 
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.CompareTag ("Player"))
+		{
+			ScoreSystem.instance.AddScore ();
+			gameObject.SetActive (false);
+		}
+	}
+}
