3201bb4a1da38d3c9bca97d6b4b9e29fada4c10e
Lisa The <lisathe97@gmail.com>
2016-05-23 10:04:20 +0200

    Add Object Spawner - Add object pool script - Add spawn manager

---
+++ BirdyRunner/Assets/Scripts/SpawnableGameObjects/Timer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer :MonoBehaviour {
+
+
+	public void ObjectTimer(float _timer, float spawnInterval)
+	{
+
+		if (_timer < spawnInterval) 
+		{
+			_timer += Time.deltaTime;
+		} 
+		else 
+		{
+			_timer = 0;
+			//SpawnObject (obstaclePool);
+		}
+	}
+}
